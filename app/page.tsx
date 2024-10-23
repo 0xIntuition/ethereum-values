9747c908dce90d4538bab0b6ee2e840fd3d986bd
Simonas Karuzas <simonas@not.cat>
2024-10-23 11:05:05 +0300

    feat: cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,28 @@
 'use client';
 
-import Balance from 'components/Balance';
-import BlockNumber from 'components/BlockNumber';
 import Button from 'components/Button';
-import ContractEvent from 'components/ContractEvent';
-import ContractRead from 'components/ContractRead';
-import ContractReads from 'components/ContractReads';
-import ContractWrite from 'components/ContractWrite';
-import EnsAddress from 'components/EnsAddress';
-import EnsAvatar from 'components/EnsAvatar';
-import EnsName from 'components/EnsName';
-import EnsResolver from 'components/EnsResolver';
-import FeeData from 'components/FeeData';
-import PublicClient from 'components/PublicClient';
-import SendTransaction from 'components/SendTransaction';
-import SignMessage from 'components/SignMessage';
-import SignTypedData from 'components/SignTypedData';
-import Signer from 'components/Signer';
 import SwitchNetwork from 'components/SwitchNetwork';
-import Token from 'components/Token';
-import Transaction from 'components/Transaction';
-import WaitForTransaction from 'components/WaitForTransaction';
-import WalletClient from 'components/WalletClient';
-import WatchPendingTransactions from 'components/WatchPendingTransactions';
-import {shorten} from 'lib/utils';
-import Image from 'next/image';
-import {useAccount, useDisconnect} from 'wagmi';
+import Triples from 'components/Triples';
+import { shorten } from 'lib/utils';
+import { useAccount, useDisconnect } from 'wagmi';
 
-import {usePrivy, useWallets} from '@privy-io/react-auth';
-import {useSetActiveWallet} from '@privy-io/wagmi';
+import { usePrivy, useWallets } from '@privy-io/react-auth';
+import { useSetActiveWallet } from '@privy-io/wagmi';
 
-import wagmiPrivyLogo from '../public/wagmi_privy_logo.png';
 
-const MonoLabel = ({label}: {label: string}) => {
+const MonoLabel = ({ label }: { label: string }) => {
   return <span className="rounded-xl bg-slate-200 px-2 py-1 font-mono">{label}</span>;
 };
 
 export default function Home() {
   // Privy hooks
-  const {ready, user, authenticated, login, connectWallet, logout, linkWallet} = usePrivy();
-  const {wallets, ready: walletsReady} = useWallets();
+  const { ready, user, authenticated, login, connectWallet, logout, linkWallet } = usePrivy();
+  const { wallets, ready: walletsReady } = useWallets();
 
   // WAGMI hooks
-  const {address, isConnected, isConnecting, isDisconnected} = useAccount();
-  const {disconnect} = useDisconnect();
-  const {setActiveWallet} = useSetActiveWallet();
+  const { address, isConnected, isConnecting, isDisconnected } = useAccount();
+  const { disconnect } = useDisconnect();
+  const { setActiveWallet } = useSetActiveWallet();
 
   if (!ready) {
     return null;
@@ -53,34 +31,7 @@ export default function Home() {
   return (
     <>
       <main className="min-h-screen bg-slate-200 p-4 text-slate-800">
-        <Image
-          className="mx-auto rounded-lg"
-          src={wagmiPrivyLogo}
-          alt="wagmi x privy logo"
-          width={400}
-          height={100}
-        />
-        <p className="my-4 text-center">
-          This demo showcases how you can integrate{' '}
-          <a href="https://wagmi.sh/" className="font-medium underline">
-            wagmi
-          </a>{' '}
-          alongside{' '}
-          <a href="https://www.privy.io/" className="font-medium underline">
-            Privy
-          </a>{' '}
-          in your React app. Login below to try it out!
-          <br />
-          For more information, check out{' '}
-          <a href="https://docs.privy.io/guide/guides/wagmi" className="font-medium underline">
-            our integration guide
-          </a>{' '}
-          or the{' '}
-          <a href="https://github.com/privy-io/wagmi-demo" className="font-medium underline">
-            source code
-          </a>{' '}
-          for this app.
-        </p>
+        <Triples />
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
           <div className="border-1 flex flex-col items-start gap-2 rounded border border-black bg-slate-100 p-3">
             <h1 className="text-4xl font-bold">Privy</h1>
@@ -152,28 +103,8 @@ export default function Home() {
                   address: <MonoLabel label={address} />
                 </p>
 
-                <Balance />
-                <Signer />
-                <SignMessage />
-                <SignTypedData />
-                <PublicClient />
-                <EnsName />
-                <EnsAddress />
-                <EnsAvatar />
-                <EnsResolver />
                 <SwitchNetwork />
-                <BlockNumber />
-                <SendTransaction />
-                <ContractRead />
-                <ContractReads />
-                <ContractWrite />
-                <ContractEvent />
-                <FeeData />
-                <Token />
-                <Transaction />
-                <WatchPendingTransactions />
-                <WalletClient />
-                <WaitForTransaction />
+
 
                 <h2 className="mt-6 text-2xl">useDisconnect</h2>
                 <Button onClick_={disconnect} cta="Disconnect from WAGMI" />
